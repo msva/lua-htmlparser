3f13077f33f8d3cc8e25d791c863d249f472368b
Vadim A. Misbakh-Soloviov <git@mva.name>
2019-06-09 20:32:07 +0300

    bumping travis

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -61,18 +61,21 @@ if [ "$LUAJIT" == "yes" ]; then
 else
 
   if [ "$LUA" == "lua5.1" ]; then
-    curl http://www.lua.org/ftp/lua-5.1.5.tar.gz | tar xz
+    suff="ftp/lua-5.1.5.tar.gz"
   elif [ "$LUA" == "lua5.2" ]; then
-    curl http://www.lua.org/ftp/lua-5.2.4.tar.gz | tar xz
+    suff="ftp/lua-5.2.4.tar.gz"
   elif [ "$LUA" == "lua5.3" ]; then
-    curl http://www.lua.org/ftp/lua-5.3.5.tar.gz | tar xz
+    suff="ftp/lua-5.3.5.tar.gz"
   elif [ "$LUA" == "lua5.4" ]; then
-    curl http://www.lua.org/work/lua-5.4.0-alpha-rc2.tar.gz | tar xz
+    suff="work/lua-5.4.0-alpha-rc2.tar.gz"
   fi
-  cd lua-5*
+
+  mkdir lua_build
+  curl -LSs http://www.lua.org/"${suff}" | tar xz -C lua_build --strip-components=1
+  cd lua_build
 
   # Build Lua without backwards compatibility for testing
-  perl -i -pe 's/-DLUA_COMPAT_(ALL|5_2)//' src/Makefile
+  perl -i -pe 's/-DLUA_COMPAT_(ALL|5_[0-9])//' src/Makefile
   make $PLATFORM
   make INSTALL_TOP="$LUA_HOME_DIR" install;
 
@@ -87,16 +90,16 @@ lua -v
 
 LUAROCKS_BASE=luarocks-$LUAROCKS
 
-curl --location http://luarocks.org/releases/$LUAROCKS_BASE.tar.gz | tar xz
+curl -LSs https://luarocks.github.io/luarocks/releases/$LUAROCKS_BASE.tar.gz | tar xz
 
 cd $LUAROCKS_BASE
 
 if [ "$LUA" == "luajit" ]; then
-  ./configure --lua-suffix=jit --with-lua-include="$LUA_HOME_DIR/include/luajit-2.0" --prefix="$LR_HOME_DIR";
+  ./configure --with-lua-include="$LUA_HOME_DIR/include/luajit-2.0" --prefix="$LR_HOME_DIR";
 elif [ "$LUA" == "luajit2.0" ]; then
-  ./configure --lua-suffix=jit --with-lua-include="$LUA_HOME_DIR/include/luajit-2.0" --prefix="$LR_HOME_DIR";
+  ./configure --with-lua-include="$LUA_HOME_DIR/include/luajit-2.0" --prefix="$LR_HOME_DIR";
 elif [ "$LUA" == "luajit2.1" ]; then
-  ./configure --lua-suffix=jit --with-lua-include="$LUA_HOME_DIR/include/luajit-2.1" --prefix="$LR_HOME_DIR";
+  ./configure --with-lua-include="$LUA_HOME_DIR/include/luajit-2.1" --prefix="$LR_HOME_DIR";
 else
   ./configure --with-lua="$LUA_HOME_DIR" --prefix="$LR_HOME_DIR"
 fi
